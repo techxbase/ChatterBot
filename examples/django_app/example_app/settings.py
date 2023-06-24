f4db260f74c5350a7b3e7334bad5e320f405cacf
techxbase <64177447+techxbase@users.noreply.github.com>
2023-06-24 14:54:02 -0700

    Update settings.py
    -Added Host

--- examples/django_app/example_app/settings.py
+++ examples/django_app/example_app/settings.py
@@ -13,7 +13,12 @@ SECRET_KEY = 'fsch+6!=q+@ol&%0x!nwdl@48^ixbd4clx5f1i!5n^66y+pmn*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  '104.236.24.119',
+  'tekhub.domcloud.io',
+  'nyc.domcloud.io']
 
 
 # Application definition
